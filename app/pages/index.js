93f4b4074dd216d0f1726a7ed76843ad229f648c
Lars Baunwall <lars.laurids.baunwall@lego.com>
2023-02-02 13:22:37 +0100

    Align header icon and text

--- app/pages/index.js
+++ app/pages/index.js
@@ -1,4 +1,4 @@
-import {useState, useRef, useEffect} from "react";
+import { useState, useRef, useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Image from "next/image";
@@ -6,227 +6,242 @@ import ReactMarkdown from "react-markdown";
 import CircularProgress from "@mui/material/CircularProgress";
 
 export default function Home() {
-    const [userInput, setUserInput] = useState("");
-    const [history, setHistory] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [messages, setMessages] = useState([
-        {
-            message: "Hi there! How can I help?",
-            type: "apiMessage",
-        },
+  const [userInput, setUserInput] = useState("");
+  const [history, setHistory] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [messages, setMessages] = useState([
+    {
+      message: "Hi there! How can I help?",
+      type: "apiMessage",
+    },
+  ]);
+
+  const messageListRef = useRef(null);
+  const textAreaRef = useRef(null);
+
+  // Auto scroll chat to bottom
+  useEffect(() => {
+    const messageList = messageListRef.current;
+    messageList.scrollTop = messageList.scrollHeight;
+  }, [messages]);
+
+  // Focus on text field on load
+  useEffect(() => {
+    textAreaRef.current.focus();
+  }, []);
+
+  // Handle errors
+  const handleError = () => {
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      {
+        message:
+          "Oops! I couldn't hear you? Maybe it's just me, but can you please repeat?",
+        type: "apiMessage",
+      },
+    ]);
+    setLoading(false);
+    setUserInput("");
+  };
+
+  // Handle form submission
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (userInput.trim() === "") {
+      return;
+    }
+
+    setLoading(true);
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { message: userInput, type: "userMessage" },
     ]);
 
-    const messageListRef = useRef(null);
-    const textAreaRef = useRef(null);
+    // Send user question and history to API
+    const response = await fetch("/api/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ question: userInput, history: history }),
+    });
 
-    // Auto scroll chat to bottom
-    useEffect(() => {
-        const messageList = messageListRef.current;
-        messageList.scrollTop = messageList.scrollHeight;
-    }, [messages]);
+    if (!response.ok) {
+      handleError();
+      return;
+    }
 
-    // Focus on text field on load
-    useEffect(() => {
-        textAreaRef.current.focus();
-    }, []);
+    // Reset user input
+    setUserInput("");
+    const data = await response.json();
 
-    // Handle errors
-    const handleError = () => {
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            {
-                message: "Oops! I couldn't hear you? Maybe it's just me, but can you please repeat?",
-                type: "apiMessage",
-            },
-        ]);
-        setLoading(false);
-        setUserInput("");
-    };
+    if (data.result.error === "Unauthorized") {
+      handleError();
+      return;
+    }
 
-    // Handle form submission
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { message: data.result.success, type: "apiMessage" },
+    ]);
+    setLoading(false);
+  };
 
-        if (userInput.trim() === "") {
-            return;
-        }
+  // Prevent blank submissions and allow for multiline input
+  const handleEnter = (e) => {
+    if (e.key === "Enter" && userInput) {
+      if (!e.shiftKey && userInput) {
+        handleSubmit(e);
+      }
+    } else if (e.key === "Enter") {
+      e.preventDefault();
+    }
+  };
 
-        setLoading(true);
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            {message: userInput, type: "userMessage"},
-        ]);
+  // Keep history in sync with messages
+  useEffect(() => {
+    if (messages.length >= 3) {
+      setHistory([
+        [
+          messages[messages.length - 2].message,
+          messages[messages.length - 1].message,
+        ],
+      ]);
+    }
+  }, [messages]);
 
-        // Send user question and history to API
-        const response = await fetch("/api/chat", {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({question: userInput, history: history}),
-        });
-
-        if (!response.ok) {
-            handleError();
-            return;
-        }
-
-        // Reset user input
-        setUserInput("");
-        const data = await response.json();
-
-        if (data.result.error === "Unauthorized") {
-            handleError();
-            return;
-        }
-
-        setMessages((prevMessages) => [
-            ...prevMessages,
-            {message: data.result.success, type: "apiMessage"},
-        ]);
-        setLoading(false);
-    };
-
-    // Prevent blank submissions and allow for multiline input
-    const handleEnter = (e) => {
-        if (e.key === "Enter" && userInput) {
-            if (!e.shiftKey && userInput) {
-                handleSubmit(e);
-            }
-        } else if (e.key === "Enter") {
-            e.preventDefault();
-        }
-    };
-
-    // Keep history in sync with messages
-    useEffect(() => {
-        if (messages.length >= 3) {
-            setHistory([
-                [
-                    messages[messages.length - 2].message,
-                    messages[messages.length - 1].message,
-                ],
-            ]);
-        }
-    }, [messages]);
-
-    return (
-        <>
-            <Head>
-                <title>Meet bricky</title>
-                <meta name="description" content="Bricky: engineering handbook bot"/>
-                <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
-            <div className={styles.topnav}>
-                <div className={styles.navlogo}>
-                    <Image
-                        src="/lego.png"
-                        alt="bricky"
-                        width="40"
-                        height="40"
-                        className={styles.boticon}
-                        priority={true}
-                    /><a href="/">Bricky</a>
-                </div>
-                <div className={styles.navlinks}></div>
-            </div>
-            <main className={styles.main}>
-                <div className={styles.cloud}>
-                    <div ref={messageListRef} className={styles.messagelist}>
-                        {messages.map((message, index) => {
-                            return (
-                                // The latest message sent by the user will be animated while waiting for a response
-                                <div
-                                    key={index}
-                                    className={
-                                        message.type === "userMessage" &&
-                                        loading &&
-                                        index === messages.length - 1
-                                            ? styles.usermessagewaiting
-                                            : message.type === "apiMessage"
-                                                ? styles.apimessage
-                                                : styles.usermessage
-                                    }
-                                >
-                                    {/* Display the correct icon depending on the message type */}
-                                    {message.type === "apiMessage" ? (
-                                        <Image
-                                            src="/lego.png"
-                                            alt="AI"
-                                            width="30"
-                                            height="30"
-                                            className={styles.boticon}
-                                            priority={true}
-                                        />
-                                    ) : (
-                                        <Image
-                                            src="/usericon.png"
-                                            alt="Me"
-                                            width="30"
-                                            height="30"
-                                            className={styles.usericon}
-                                            priority={true}
-                                        />
-                                    )}
-                                    <div className={styles.markdownanswer}>
-                                        {/* Messages are being rendered in Markdown format */}
-                                        <ReactMarkdown linkTarget={"_blank"}>
-                                            {message.message}
-                                        </ReactMarkdown>
-                                    </div>
-                                </div>
-                            );
-                        })}
-                    </div>
-                </div>
-                <div className={styles.center}>
-                    <div className={styles.cloudform}>
-                        <form onSubmit={handleSubmit}>
-              <textarea
-                  disabled={loading}
-                  onKeyDown={handleEnter}
-                  ref={textAreaRef}
-                  autoFocus={false}
-                  rows={1}
-                  maxLength={512}
-                  type="text"
-                  id="userInput"
-                  name="userInput"
-                  placeholder={
-                      loading ? "Waiting for response..." : "Type your question..."
+  return (
+    <>
+      <Head>
+        <title>Meet bricky</title>
+        <meta name="description" content="Bricky: engineering handbook bot" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className={styles.topnav}>
+        <div className={styles.navlogo}>
+          <Image
+            src="/lego.png"
+            alt="bricky"
+            width="40"
+            height="40"
+            className={styles.boticon}
+            priority={true}
+          />
+          <a href="/">Ask Bricky</a>
+        </div>
+        <div className={styles.navlinks}></div>
+      </div>
+      <main className={styles.main}>
+        <div className={styles.cloud}>
+          <div ref={messageListRef} className={styles.messagelist}>
+            {messages.map((message, index) => {
+              return (
+                // The latest message sent by the user will be animated while waiting for a response
+                <div
+                  key={index}
+                  className={
+                    message.type === "userMessage" &&
+                    loading &&
+                    index === messages.length - 1
+                      ? styles.usermessagewaiting
+                      : message.type === "apiMessage"
+                      ? styles.apimessage
+                      : styles.usermessage
                   }
-                  value={userInput}
-                  onChange={(e) => setUserInput(e.target.value)}
-                  className={styles.textarea}
-              />
-                            <button
-                                type="submit"
-                                disabled={loading}
-                                className={styles.generatebutton}
-                            >
-                                {loading ? (
-                                    <div className={styles.loadingwheel}>
-                                        <CircularProgress color="inherit" size={20}/>{" "}
-                                    </div>
-                                ) : (
-                                    // Send icon SVG in input field
-                                    <svg
-                                        viewBox="0 0 20 20"
-                                        className={styles.svgicon}
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path
-                                            d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
-                                    </svg>
-                                )}
-                            </button>
-                        </form>
-                    </div>
-                    <div className={styles.footer}>
-                        <p>Built by <a href="https://github.com/larsbaunwall" target="_blank">@baunwall</a> with inspiration from <a href="https://github.com/zahidkhawaja/langchain-chat-nextjs" target="_blank">langchain-chat</a>.</p>                    </div>
+                >
+                  {/* Display the correct icon depending on the message type */}
+                  {message.type === "apiMessage" ? (
+                    <Image
+                      src="/lego.png"
+                      alt="AI"
+                      width="30"
+                      height="30"
+                      className={styles.boticon}
+                      priority={true}
+                    />
+                  ) : (
+                    <Image
+                      src="/usericon.png"
+                      alt="Me"
+                      width="30"
+                      height="30"
+                      className={styles.usericon}
+                      priority={true}
+                    />
+                  )}
+                  <div className={styles.markdownanswer}>
+                    {/* Messages are being rendered in Markdown format */}
+                    <ReactMarkdown linkTarget={"_blank"}>
+                      {message.message}
+                    </ReactMarkdown>
+                  </div>
                 </div>
-            </main>
-        </>
-    );
+              );
+            })}
+          </div>
+        </div>
+        <div className={styles.center}>
+          <div className={styles.cloudform}>
+            <form onSubmit={handleSubmit}>
+              <textarea
+                disabled={loading}
+                onKeyDown={handleEnter}
+                ref={textAreaRef}
+                autoFocus={false}
+                rows={1}
+                maxLength={512}
+                type="text"
+                id="userInput"
+                name="userInput"
+                placeholder={
+                  loading ? "Waiting for response..." : "Type your question..."
+                }
+                value={userInput}
+                onChange={(e) => setUserInput(e.target.value)}
+                className={styles.textarea}
+              />
+              <button
+                type="submit"
+                disabled={loading}
+                className={styles.generatebutton}
+              >
+                {loading ? (
+                  <div className={styles.loadingwheel}>
+                    <CircularProgress color="inherit" size={20} />{" "}
+                  </div>
+                ) : (
+                  // Send icon SVG in input field
+                  <svg
+                    viewBox="0 0 20 20"
+                    className={styles.svgicon}
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+                  </svg>
+                )}
+              </button>
+            </form>
+          </div>
+          <div className={styles.footer}>
+            <p>
+              Built by{" "}
+              <a href="https://github.com/larsbaunwall" target="_blank">
+                @baunwall
+              </a>{" "}
+              with inspiration from{" "}
+              <a
+                href="https://github.com/zahidkhawaja/langchain-chat-nextjs"
+                target="_blank"
+              >
+                langchain-chat
+              </a>
+              .
+            </p>{" "}
+          </div>
+        </div>
+      </main>
+    </>
+  );
 }
